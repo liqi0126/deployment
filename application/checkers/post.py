555708cb2d1d083baf96d2fe7387e3939d8c6289
liqi17-thu <624275030@qq.com>
2020-10-23 12:49:54 +0800

    part 1

--- application/checkers/post.py
+++ application/checkers/post.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
 
+
 def post_params_check(content):
     """
     TODO: 发帖参数检查
     """
+
+    title = content.get('content', None)
+
+    if type(title) is not str:
+        return 'title', False
+
+    if len(title) < 1 or len(title) > 64:
+        return 'title', False
+
+    post_conent = content.get('content', None)
+
+    if type(post_conent) is not str:
+        return 'content', False
+
+    if len(post_conent) < 15 or len(post_conent) > 256:
+        return 'content', False
+
     return "ok", True
--- application/checkers/reply.py
+++ application/checkers/reply.py
@@ -4,4 +4,21 @@ def reply_post_params_check(content):
     """
     TODO: 回帖参数检查
     """
+
+    reply_content = content.get('content', None)
+
+    if type(reply_content) is not str:
+        return 'content', False
+
+    if len(reply_content) < 15 or len(reply_content) > 256:
+        return 'content', False
+
+    reply_id = content.get('replyId', 0)
+
+    if type(reply_id) is not int:
+        return 'replyId', False
+
+    if reply_id < 0:
+        return 'replyId', False
+
     return "ok", True
